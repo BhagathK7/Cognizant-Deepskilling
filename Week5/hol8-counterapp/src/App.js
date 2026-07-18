e5f7189e1887a934a2c37dd8d0bbfda0818272fd
BhagathK7 <bhagathkrishnasunilkumar.23cse@kongu.edu>
2026-07-18 14:54:50 +0530

    Week5 HOL8 - Counter App using State

---
+++ Week5/hol8-counterapp/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import CountPeople from "./Components/CountPeople";
+
+function App() {
+
+    return (
+
+        <div>
+
+            <CountPeople />
+
+        </div>
+
+    );
+
+}
+
+export default App;
