16a01cbaa474a71ceed503b3bec3921da707173d
BhagathK7 <bhagathkrishnasunilkumar.23cse@kongu.edu>
2026-07-20 14:13:03 +0530

    Week5 HOL11 - Event Examples App

--- Week5/hol11-eventexamplesapp/src/App.js
+++ Week5/hol11-eventexamplesapp/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import EventExamples from "./Components/EventExamples";
+import CurrencyConvertor from "./Components/CurrencyConvertor";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+
+        <div className="App">
+
+            <h1>Event Examples App</h1>
+
+            <EventExamples />
+
+            <hr />
+
+            <CurrencyConvertor />
+
+        </div>
+
+    );
+
 }
 
 export default App;
